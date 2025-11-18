5494f60073c8209deaa9123a9def2814a6e75ef8
Kaushal Kumar <kosu1628015@gmail.com>
2025-11-18 08:43:36 +0530

    Update command.java

--- command.java
+++ command.java
@@ -1,10 +1,11 @@
-// public class command {
+public class command {
 
-//     public static void main(String[] args) {
-//         System.out.println("number of argumrnts:"+args.length);
-//         for(int i=0;i<args.length;i++){
-//             System.out.println("argument"+i);
-//         }
-//     }
-// }
-// wap a java program sum of 3 digits number without using condtiionals and  control stattements
+    public static void main(String[] args) {
+        System.out.println("number of argumrnts:"+args.length);
+        for(int i=0;i<args.length;i++){
+            System.out.println("argument"+i);
+        }
+    }
+}
+
+wap a java program sum of 3 digits number without using condtiionals and  control stattements
